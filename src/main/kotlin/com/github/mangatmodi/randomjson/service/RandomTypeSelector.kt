48b8148c23533858dc066604b0d6f8f4d2febee2
mangatmodi <mangatmodi@gmail.com>
2019-04-24 10:55:21 +0200

    Adds read me

--- src/main/kotlin/com/github/mangatmodi/randomjson/service/RandomTypeSelector.kt
+++ src/main/kotlin/com/github/mangatmodi/randomjson/service/RandomTypeSelector.kt
@@ -1,10 +1,40 @@
 package com.github.mangatmodi.randomjson.service
 
+import com.github.mangatmodi.randomjson.entity.BooleanType
+import com.github.mangatmodi.randomjson.entity.DoubleType
+import com.github.mangatmodi.randomjson.entity.IntType
 import com.github.mangatmodi.randomjson.entity.JsonDataType
+import com.github.mangatmodi.randomjson.entity.StringType
+import java.util.concurrent.ThreadLocalRandom
+
 /**
  * Used by [com.github.mangatmodi.randomjson.service.impl.SimpleJsonCreator]. Selects the JSON type to be used
  * for next field in the random json string
  * */
 interface RandomTypeSelector {
     fun select(): JsonDataType
+
+    companion object {
+        @JvmStatic
+            /**
+             * Select a json value of type Integer, Double, String, Boolean.
+             * Select any of them with equal probability
+             * */
+        fun default(): RandomTypeSelector = RandomTypeSelectorNaiveImpl()
+    }
+
+    private class RandomTypeSelectorNaiveImpl : RandomTypeSelector {
+        override fun select(): JsonDataType {
+            val rand = ThreadLocalRandom
+                .current()
+                .nextInt(4)
+            return when (rand) {
+                0 -> IntType
+                1 -> DoubleType
+                2 -> StringType
+                3 -> BooleanType
+                else -> StringType
+            }
+        }
+    }
 }
