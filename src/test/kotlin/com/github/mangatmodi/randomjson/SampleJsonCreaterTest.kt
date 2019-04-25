b9f13d0183ed239db7f92d1733333d153ddc55d9
mangatmodi <mangatmodi@gmail.com>
2019-04-25 17:17:56 +0200

    AFixes JSON array creation

--- src/test/kotlin/com/github/mangatmodi/randomjson/SampleJsonCreaterTest.kt
+++ src/test/kotlin/com/github/mangatmodi/randomjson/SampleJsonCreaterTest.kt
@@ -12,31 +12,19 @@ class SampleJsonCreatorTest : ShouldSpec() {
                     val jsonString = RandomJsonCreator.fromSampleString(
                         """
 {
-	"id": "0001",
-	"type": "donut",
-	"name": "Cake",
-	"ppu": 0.55,
-	"batters":
-		{
-			"batter":
-				[
-					{ "id": "1001", "type": "Regular" },
-					{ "id": "1002", "type": "Chocolate" },
-					{ "id": "1003", "type": "Blueberry" },
-					{ "id": "1004", "type": "Devil's Food" }
-				]
-		},
-	"topping":
+	"id": 23,
+	"CGPA": 4.78,
+	"name": "Mangat",
+	"date": "2019-04-25 10:01:01 [UTC]",
+	"courses":
 		[
-			{ "id": "5001", "type": "None" },
-			{ "id": "5002", "type": "Glazed" },
-			{ "id": "5005", "type": "Sugar" },
-			{ "id": "5007", "type": "Powdered Sugar" },
-			{ "id": "5006", "type": "Chocolate with Sprinkles" },
-			{ "id": "5003", "type": "Chocolate" },
-			{ "id": "5004", "type": "Maple" }
+			{ "id": "1", "name": "Algorithms", "score":"3.5" },
+			{ "id": "2", "type": "Functional Programming", "score":"4.1" },
+			{ "id": "3", "type": "Linear Alegbra", "score":"5.0" },
+			{ "id": "4", "type": "Data Streaming", "score":"5.0" }
 		]
-} """.trimIndent(),
+}
+ """.trimIndent(),
                         this.config()
                     ).create()
                     println(jsonString)
---
+++ src/test/kotlin/com/github/mangatmodi/randomjson/exampleGenerator.kt
@@ -0,0 +1,37 @@
+package com.github.mangatmodi.randomjson
+
+import com.github.mangatmodi.randomjson.config.RandomJsonConfig
+import com.github.mangatmodi.randomjson.service.RandomBoolean
+import com.github.mangatmodi.randomjson.service.RandomDouble
+import com.github.mangatmodi.randomjson.service.RandomInt
+import com.github.mangatmodi.randomjson.service.RandomString
+
+fun main(args: Array<String>) {
+    val config = RandomJsonConfig(
+        RandomDouble.threadLocalRandom(),
+        RandomInt.threadLocalRandom(),
+        RandomString.charArray("eusbwopw".toCharArray(), 4),
+        RandomBoolean.uniform(),
+        RandomString.charArray("abcdefg".toCharArray(), 3)
+    )
+    val input = """
+            {
+        "id": 23,
+        "CGPA": 4.78,
+        "name": "Mangat",
+        "date": "2019-04-25 10:01:01 [UTC]",
+        "courses":
+            [
+                { "id": 1, "name": "Algorithms", "score":3.5 },
+                { "id": 2, "type": "Functional Programming", "score":4.1 },
+                { "id": 3, "type": "Linear Alegbra", "score":5.0 },
+                { "id": 4, "type": "Data Streaming", "score":5.0 }
+            ]
+    }
+
+    """.trimIndent()
+
+    val jsonCreater = RandomJsonCreator
+        .fromSampleString(input, config)
+    println(jsonCreater.create())
+}
